0af9fefee19cb3673b875a9ad5ef4393dc497a84
mikelove <michaelisaiahlove@gmail.com>
2016-10-12 11:51:42 -0400

    plots

--- Rscripts/analyze.R
+++ Rscripts/analyze.R
@@ -1,4 +1,16 @@
 file <- "../qcstats/GD667.QCstats.txt"
 library(readr)
 qc <- read_delim(file,"\t")
-dim(qc)
+library(rafalib)
+means <- sapply(split(qc$GC_hist_bins_70, qc$Operator), mean)
+o <- order(means)
+qc$Operator <- factor(qc$Operator)
+qc$Operator <- factor(qc$Operator, levels=levels(qc$Operator)[o])
+lvls <- levels(qc$Operator)
+levels(qc$Operator)[lvls == "MPIMG_S_DOKEL"] <- "MPIMG"
+bigpar(3,3,mar=c(8,4,1,1))
+for (i in 12:19) {
+  boxplot(qc[[paste0("GC_hist_bins_",i*5)]] ~ qc$Operator,
+          las=3)
+  legend("topleft",legend=paste0("GC=",i*5),bty="n",pch=NA,cex=2)
+}
